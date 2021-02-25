94348129c169478d303d142f465dd83a54443b7d
MungC2010G2 <zhaiduong26z@gmail.com>
2021-02-25 09:40:07 +0700

    finish method showAllProduct

---
+++ src/main/java/controller/ProductServlet.java
@@ -0,0 +1,51 @@
+package controller;
+
+import model.Category;
+import model.Product;
+import service.CategoryService;
+import service.ProductService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProductServlet", urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+    ProductService productService = new ProductService();
+    CategoryService categoryService = new CategoryService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "":
+                showAllProduct(request, response);
+                break;
+        }
+    }
+
+    private void showAllProduct(HttpServletRequest request, HttpServletResponse response) {
+        List<Product> products = productService.fillAll();
+        request.setAttribute("products",products);
+        List<Category> categories = categoryService.fillAll();
+        RequestDispatcher dispatcher = request.getRequestDispatcher("ManagerProduct.jsp");
+        try {
+            dispatcher.forward(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+}
